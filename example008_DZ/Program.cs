458564102213246e4c8c74903b0e6cd280d8084b
GalinaAlnichenkova <g.alnichenkova@gmail.com>
2022-08-04 21:52:46 +0400

    add example008 DZ

---
+++ example008_DZ/Program.cs
@@ -0,0 +1,26 @@
+﻿int count = 0;
+int distans = 10000;
+int SF1 = 1;
+int SF2 = 2;
+int Fr = 2;
+int DS = 5;
+int time = 0;
+
+while(distans>10)
+{
+    if(Fr==1)
+    {
+        time = distans/(SF1 + DS);
+        Fr = 2;
+    } 
+    else {
+        time = distans/(SF2 + DS);
+        Fr = 1;
+    }
+
+    distans = distans - (SF1 + SF2) * time;
+    count = count + 1;
+}
+Console.WriteLine(count);
+
+
